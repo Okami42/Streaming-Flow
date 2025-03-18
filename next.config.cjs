92217e1e0f3a9834f750d9d12e6b525faab69f97
Okami42 <95964075+Okami42@users.noreply.github.com>
2025-03-19 00:14:06 +0100

    Add files via upload

--- next.config.cjs
+++ next.config.cjs
@@ -45,14 +45,6 @@ const nextConfig = {
         source: '/(.*)',
         headers: [
           {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin',
-          },
-          {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
-          },
-          {
             key: 'Access-Control-Allow-Origin',
             value: '*'
           }
