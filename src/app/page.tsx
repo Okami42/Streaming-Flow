0290b739dffbd44e3cc289a9ef6ae4aa19a0f8a4
Okami42 <95964075+Okami42@users.noreply.github.com>
2025-07-05 09:14:11 +0200

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Link from "next/link";
-import { motion } from "framer-motion";
 import CustomImage from "@/components/ui/custom-image";
 
 <meta name="google-site-verification" content="JXUG7kpK-JLc6mNdI8pFXnTQPnJA4Z-TKV4iefh1aAg" />
