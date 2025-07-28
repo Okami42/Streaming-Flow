927f05f8396bf5b3151450c6921ab4c79ecd4ee9
Okami42 <95964075+Okami42@users.noreply.github.com>
2025-07-28 03:40:03 +0200

    Delete src directory

--- src/components/ui/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as HLSPlayer } from './hls-player';
-export { default as CustomImage } from './custom-image';
-export { default as VideoPlayer } from './video-player';
-
-// ShadCN components
-export * from './button';
-export * from './tabs';
-// Add other component exports as needed 
