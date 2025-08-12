615134737098a2f83e2d4c3b690cc709b6478b0f
Okami42 <95964075+Okami42@users.noreply.github.com>
2025-08-12 14:50:46 +0200

    Delete src directory

--- src/lib/empty-module.js
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-// Module vide pour remplacer hls.js dans les builds serveur
-const emptyModule = {};
-=======
-// Module vide pour remplacer hls.js dans les builds serveur
-const emptyModule = {};
->>>>>>> 975648a9ed94bfc5da709a184d4431ff9a4d85b3
-export default emptyModule; 
