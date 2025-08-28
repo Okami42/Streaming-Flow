c20306cd51b41131b4cfc7528fd3b89f3b655609
Okami42 <95964075+Okami42@users.noreply.github.com>
2025-08-28 21:10:14 +0200

    Update episodes_vostfr.js

--- public/anime_episodes_js/a-couple-of-cuckoos/saison2/episodes_vostfr.js
+++ public/anime_episodes_js/a-couple-of-cuckoos/saison2/episodes_vostfr.js
@@ -7,15 +7,13 @@
 var eps3 = [
 
 'https://video.sibnet.ru/shell.php?videoid=5957240',
-
 'https://video.sibnet.ru/shell.php?videoid=5965135',
-
 'https://video.sibnet.ru/shell.php?videoid=5971799',
-
 'https://video.sibnet.ru/shell.php?videoid=5977788',
-
 'https://video.sibnet.ru/shell.php?videoid=5983640',
-
+'https://video.sibnet.ru/shell.php?videoid=5989361',
+'https://video.sibnet.ru/shell.php?videoid=5993640',
+'https://video.sibnet.ru/shell.php?videoid=5998930',
 
 
 ];
