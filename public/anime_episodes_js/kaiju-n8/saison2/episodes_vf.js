a38d6dbbf8722119ff446532a679718f7c9b42c8
Okami42 <95964075+Okami42@users.noreply.github.com>
2025-09-13 18:16:47 +0200

    Update episodes_vf.js

--- public/anime_episodes_js/kaiju-n8/saison2/episodes_vf.js
+++ public/anime_episodes_js/kaiju-n8/saison2/episodes_vf.js
@@ -7,12 +7,14 @@
 var eps3 = [
 
 'https://video.sibnet.ru/shell.php?videoid=5968861',
-
 'https://video.sibnet.ru/shell.php?videoid=5975339',
-
 'https://video.sibnet.ru/shell.php?videoid=5980803',
-
-'https://vidmoly.to/embed-vdkat6bk4md4.html',
+'https://sendvid.com/embed/ihaj6og7',
+'https://video.sibnet.ru/shell.php?videoid=5991681',
+'https://video.sibnet.ru/shell.php?videoid=5996710',
+'https://video.sibnet.ru/shell.php?videoid=6002058',
+'https://video.sibnet.ru/shell.php?videoid=6012397',
+'https://video.sibnet.ru/shell.php?videoid=6012403',
 
 
 
