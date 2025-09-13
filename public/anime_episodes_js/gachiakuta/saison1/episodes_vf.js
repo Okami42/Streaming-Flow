0a395eaf4b998cdad305b73a31110160fe2b1b1a
Okami42 <95964075+Okami42@users.noreply.github.com>
2025-09-13 18:15:02 +0200

    Update episodes_vf.js

--- public/anime_episodes_js/gachiakuta/saison1/episodes_vf.js
+++ public/anime_episodes_js/gachiakuta/saison1/episodes_vf.js
@@ -7,14 +7,14 @@
 var eps3 = [
 
 'https://video.sibnet.ru/shell.php?videoid=5955316',
-
 'https://video.sibnet.ru/shell.php?videoid=5963187',
-
 'https://video.sibnet.ru/shell.php?videoid=5976520',
-
 'https://video.sibnet.ru/shell.php?videoid=5981805',
-
 'https://video.sibnet.ru/shell.php?videoid=5988006',
+'https://video.sibnet.ru/shell.php?videoid=5992415',
+'https://video.sibnet.ru/shell.php?videoid=5997438',
+'https://video.sibnet.ru/shell.php?videoid=6002913',
+'https://video.sibnet.ru/shell.php?videoid=6011376',
 
 
 
