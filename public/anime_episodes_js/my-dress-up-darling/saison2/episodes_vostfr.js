01dd68ed080736b3b28f18e70e7941746615c217
Okami42 <95964075+Okami42@users.noreply.github.com>
2025-09-13 18:22:25 +0200

    Update episodes_vostfr.js

--- public/anime_episodes_js/my-dress-up-darling/saison2/episodes_vostfr.js
+++ public/anime_episodes_js/my-dress-up-darling/saison2/episodes_vostfr.js
@@ -7,16 +7,15 @@
 var eps3 = [
 
 'https://video.sibnet.ru/shell.php?videoid=5953996',
-
 'https://video.sibnet.ru/shell.php?videoid=5962013',
-
 'https://video.sibnet.ru/shell.php?videoid=5968922',
-
 'https://video.sibnet.ru/shell.php?videoid=5975436',
-
 'https://video.sibnet.ru/shell.php?videoid=5980857',
-
 'https://video.sibnet.ru/shell.php?videoid=5987292',
+'https://video.sibnet.ru/shell.php?videoid=5991754',
+'https://video.sibnet.ru/shell.php?videoid=5996811',
+'https://video.sibnet.ru/shell.php?videoid=6002141',
+'https://video.sibnet.ru/shell.php?videoid=6011157',
 
 
 
