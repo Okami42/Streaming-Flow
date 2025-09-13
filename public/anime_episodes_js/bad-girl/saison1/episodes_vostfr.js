1ce2be06a5ad6a8d428d2dd8fafb9c9bdea8baa8
Okami42 <95964075+Okami42@users.noreply.github.com>
2025-09-13 13:21:29 +0200

    Update episodes_vostfr.js

--- public/anime_episodes_js/bad-girl/saison1/episodes_vostfr.js
+++ public/anime_episodes_js/bad-girl/saison1/episodes_vostfr.js
@@ -7,16 +7,15 @@
 var eps3 = [
 
 'https://video.sibnet.ru/shell.php?videoid=5956062',
-
 'https://video.sibnet.ru/shell.php?videoid=5961978',
-
 'https://video.sibnet.ru/shell.php?videoid=5968867',
-
 'https://video.sibnet.ru/shell.php?videoid=5975362',
-
 'https://video.sibnet.ru/shell.php?videoid=5980856',
-
 'https://video.sibnet.ru/shell.php?videoid=5987233',
+'https://video.sibnet.ru/shell.php?videoid=5991711',
+'https://video.sibnet.ru/shell.php?videoid=5996719',
+'https://video.sibnet.ru/shell.php?videoid=6002091',
+'https://video.sibnet.ru/shell.php?videoid=6011151',
 
 
 
