dd4131a78967062cad5abb1578e1b4710e058763
Okami42 <95964075+Okami42@users.noreply.github.com>
2025-09-13 13:54:02 +0200

    Add files via upload

--- public/anime_episodes_js/busu-ni-hanataba-wo/saison1/episodes_vostfr.js
+++ public/anime_episodes_js/busu-ni-hanataba-wo/saison1/episodes_vostfr.js
@@ -7,16 +7,16 @@
 var eps3 = [
 
 'https://video.sibnet.ru/shell.php?videoid=5953084',
-
 'https://video.sibnet.ru/shell.php?videoid=5960710',
-
 'https://video.sibnet.ru/shell.php?videoid=5967614',
-
 'https://video.sibnet.ru/shell.php?videoid=5974241',
-
 'https://video.sibnet.ru/shell.php?videoid=5979996',
-
 'https://video.sibnet.ru/shell.php?videoid=5986298',
+'https://video.sibnet.ru/shell.php?videoid=5991033',
+'https://video.sibnet.ru/shell.php?videoid=5995879',
+'https://video.sibnet.ru/shell.php?videoid=6001156',
+'https://video.sibnet.ru/shell.php?videoid=6011128',
+'https://video.sibnet.ru/shell.php?videoid=6011642',
 
 
 
