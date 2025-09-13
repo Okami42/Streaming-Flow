bb9e5aa6acef2dfae1aea7c55fed82bf3620a829
Okami42 <95964075+Okami42@users.noreply.github.com>
2025-09-13 18:16:03 +0200

    Update episodes_vostfr.js

--- public/anime_episodes_js/kaiju-n8/saison2/episodes_vostfr.js
+++ public/anime_episodes_js/kaiju-n8/saison2/episodes_vostfr.js
@@ -7,13 +7,14 @@
 var eps3 = [
 
 'https://video.sibnet.ru/shell.php?videoid=5968853',
-
 'https://video.sibnet.ru/shell.php?videoid=5975315',
-
 'https://video.sibnet.ru/shell.php?videoid=5980756',
-
 'https://video.sibnet.ru/shell.php?videoid=5987194',
-
+'https://video.sibnet.ru/shell.php?videoid=5991616',
+'https://video.sibnet.ru/shell.php?videoid=5996669',
+'https://video.sibnet.ru/shell.php?videoid=6002033',
+'https://video.sibnet.ru/shell.php?videoid=6011153',
+'https://video.sibnet.ru/shell.php?videoid=6012353',
 
 
 ];
