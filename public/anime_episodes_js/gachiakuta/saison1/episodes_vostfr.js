6cbfbc4a793c66da0ad81d95c48d22eebd970a1e
Okami42 <95964075+Okami42@users.noreply.github.com>
2025-09-13 18:14:28 +0200

    Update episodes_vostfr.js

--- public/anime_episodes_js/gachiakuta/saison1/episodes_vostfr.js
+++ public/anime_episodes_js/gachiakuta/saison1/episodes_vostfr.js
@@ -7,14 +7,14 @@
 var eps3 = [
 
 'https://video.sibnet.ru/shell.php?videoid=5955294',
-
 'https://video.sibnet.ru/shell.php?videoid=5963148',
-
 'https://video.sibnet.ru/shell.php?videoid=5976506',
-
 'https://video.sibnet.ru/shell.php?videoid=5981768',
-
 'https://video.sibnet.ru/shell.php?videoid=5987949',
+'https://video.sibnet.ru/shell.php?videoid=5992394',
+'https://video.sibnet.ru/shell.php?videoid=5997416',
+'https://video.sibnet.ru/shell.php?videoid=6002915',
+'https://video.sibnet.ru/shell.php?videoid=6011378',
 
 
 
