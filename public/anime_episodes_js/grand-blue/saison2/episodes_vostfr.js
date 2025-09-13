483fe32a77d5b1b801fe8d3858b2fc6ccb2d1d7f
Okami42 <95964075+Okami42@users.noreply.github.com>
2025-09-13 17:56:10 +0200

    Add files via upload

--- public/anime_episodes_js/grand-blue/saison2/episodes_vostfr.js
+++ public/anime_episodes_js/grand-blue/saison2/episodes_vostfr.js
@@ -7,14 +7,15 @@
 var eps3 = [
 
 'https://video.sibnet.ru/shell.php?videoid=5956455',
-
 'https://video.sibnet.ru/shell.php?videoid=5964156',
-
 'https://video.sibnet.ru/shell.php?videoid=5970997',
-
 'https://video.sibnet.ru/shell.php?videoid=5977206',
-
 'https://video.sibnet.ru/shell.php?videoid=5982834',
+'https://video.sibnet.ru/shell.php?videoid=5988631',
+'https://video.sibnet.ru/shell.php?videoid=5993088',
+'https://video.sibnet.ru/shell.php?videoid=5998161',
+'https://video.sibnet.ru/shell.php?videoid=6003429',
+'https://video.sibnet.ru/shell.php?videoid=6011739',
 
 
 
