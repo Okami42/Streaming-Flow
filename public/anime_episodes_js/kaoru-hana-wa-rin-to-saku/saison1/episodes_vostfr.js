429f24fa085727e55abaf1f6129a2ef41c55cd43
Okami42 <95964075+Okami42@users.noreply.github.com>
2025-09-13 18:11:01 +0200

    Add files via upload

--- public/anime_episodes_js/kaoru-hana-wa-rin-to-saku/saison1/episodes_vostfr.js
+++ public/anime_episodes_js/kaoru-hana-wa-rin-to-saku/saison1/episodes_vostfr.js
@@ -7,15 +7,15 @@
 var eps3 = [
 
 'https://video.sibnet.ru/shell.php?videoid=5954978',
-
 'https://video.sibnet.ru/shell.php?videoid=5963238',
-
 'https://video.sibnet.ru/shell.php?videoid=5970235',
-
 'https://video.sibnet.ru/shell.php?videoid=5976752',
-
 'https://video.sibnet.ru/shell.php?videoid=5981911',
-
+'https://video.sibnet.ru/shell.php?videoid=5990112',
+'https://video.sibnet.ru/shell.php?videoid=5992625',
+'https://video.sibnet.ru/shell.php?videoid=5997593',
+'https://video.sibnet.ru/shell.php?videoid=6003085',
+'https://sendvid.com/embed/cgsyyble,'
 
 
 ];
