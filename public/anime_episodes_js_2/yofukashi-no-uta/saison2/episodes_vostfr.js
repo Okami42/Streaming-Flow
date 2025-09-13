7eef9cdee90785265768c1cb77c098528e61fde4
Okami42 <95964075+Okami42@users.noreply.github.com>
2025-09-13 18:19:39 +0200

    Update episodes_vostfr.js

--- public/anime_episodes_js_2/yofukashi-no-uta/saison2/episodes_vostfr.js
+++ public/anime_episodes_js_2/yofukashi-no-uta/saison2/episodes_vostfr.js
@@ -7,17 +7,16 @@
 var eps3 = [
 
 'https://video.sibnet.ru/shell.php?videoid=5953126',
-
 'https://video.sibnet.ru/shell.php?videoid=5960806',
-
 'https://video.sibnet.ru/shell.php?videoid=5967710',
-
 'https://video.sibnet.ru/shell.php?videoid=5974336',
-
 'https://video.sibnet.ru/shell.php?videoid=5980101',
-
 'https://video.sibnet.ru/shell.php?videoid=5986504',
-
+'https://video.sibnet.ru/shell.php?videoid=5991108',
+'https://video.sibnet.ru/shell.php?videoid=5995963',
+'https://video.sibnet.ru/shell.php?videoid=6001269',
+'https://video.sibnet.ru/shell.php?videoid=6011134',
+'https://video.sibnet.ru/shell.php?videoid=6011686',
 
 
 ];
