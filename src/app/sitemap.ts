ab0de5a67d58d62a21945c5ec6319cf59ebcdeca
Okami42 <95964075+Okami42@users.noreply.github.com>
2025-09-17 17:29:41 +0200

    Add files via upload

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,55 +9,46 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: baseUrl,
       lastModified: new Date(),
-      changeFrequency: 'daily',
       priority: 1,
     },
     {
       url: `${baseUrl}/catalogue`,
       lastModified: new Date(),
-      changeFrequency: 'daily',
       priority: 0.9,
     },
     {
       url: `${baseUrl}/anime`,
       lastModified: new Date(),
-      changeFrequency: 'daily',
       priority: 0.9,
     },
     {
       url: `${baseUrl}/series`,
       lastModified: new Date(),
-      changeFrequency: 'daily',
       priority: 0.9,
     },
     {
       url: `${baseUrl}/series/catalogue`,
       lastModified: new Date(),
-      changeFrequency: 'daily',
       priority: 0.8,
     },
     {
       url: `${baseUrl}/search`,
       lastModified: new Date(),
-      changeFrequency: 'weekly',
       priority: 0.7,
     },
     {
       url: `${baseUrl}/series/search`,
       lastModified: new Date(),
-      changeFrequency: 'weekly',
       priority: 0.7,
     },
     {
       url: `${baseUrl}/planning`,
       lastModified: new Date(),
-      changeFrequency: 'daily',
       priority: 0.6,
     },
     {
       url: `${baseUrl}/series/planning`,
       lastModified: new Date(),
-      changeFrequency: 'daily',
       priority: 0.6,
     }
   ]
@@ -66,7 +57,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
   const animePages: MetadataRoute.Sitemap = animes.map((anime) => ({
     url: `${baseUrl}/catalogue/${anime.id}`,
     lastModified: new Date(),
-    changeFrequency: 'weekly' as const,
     priority: 0.8,
   }))
 
